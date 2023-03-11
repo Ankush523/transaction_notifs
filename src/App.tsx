d7cc3775d35d93c3875e2a4be32d898c36300fe6
Ankush523 <ankush.ad050203@gmail.com>
2023-03-11 10:36:54 +0530

    ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <Homepage/>
-      {/* <Linkpage/> */}
-      {/* <Completion/> */}
+      {/* <Linkpage/>
+      <Completion/> */}
     </div>
   );
 }
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,6 +4,10 @@ const Homepage = () => {
   return (
     <div className='flex flex-col justify-center border border-purple3 p-[8%]'>
         <label className='text-purple2 text-base text-left'>Get Notified when your funds go through ? </label>
+        <div className='flex flex-row  text-blue1'>
+            <input type="radio" value="Yes"/>Yes
+            <input type="radio" value="No"/>No
+        </div>
         <button className='text-blue1 mt-[10%] text-left'>Yes</button>
         <button className='text-blue1 mt-[5%] text-left'>No</button>
         <label className='text-grey1 mt-[15%]'>@Powered by glyde</label>
