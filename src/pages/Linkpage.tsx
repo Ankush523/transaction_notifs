3d74b28610d6759fe49d34e0ca95c65e5c8a8ba2
Ankush523 <ankush.ad050203@gmail.com>
2023-03-14 11:16:39 +0530

    reverted to previous code

--- src/pages/Linkpage.tsx
+++ src/pages/Linkpage.tsx
@@ -1,33 +1,33 @@
 import React from 'react'
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-interface EthereumWindow {
-    ethereum?: {
-      request: (args: { method: string; }) => Promise<string[]>;
-    };
-  }
+// interface EthereumWindow {
+//     ethereum?: {
+//       request: (args: { method: string; }) => Promise<string[]>;
+//     };
+//   }
 
 const Linkpage = () => {
-    const [address, setAddress] = useState<string | null>(null);
+    // const [address, setAddress] = useState<string | null>(null);
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-          const { ethereum } = window as EthereumWindow;
-          if (ethereum) {
-            ethereum.request({ method: 'eth_requestAccounts' })
-              .then(accounts => {
-                setAddress(accounts[0]);
-                console.log(accounts[0])
-              })
-              .catch(error => {
-                console.log(error);
-              });
-          }
-        }
-        else{
-            console.log("error")
-        }
-      }, []);
+    // useEffect(() => {
+    //     if (typeof window !== 'undefined') {
+    //       const { ethereum } = window as EthereumWindow;
+    //       if (ethereum) {
+    //         ethereum.request({ method: 'eth_requestAccounts' })
+    //           .then(accounts => {
+    //             setAddress(accounts[0]);
+    //             console.log(accounts[0])
+    //           })
+    //           .catch(error => {
+    //             console.log(error);
+    //           });
+    //       }
+    //     }
+    //     else{
+    //         console.log("error")
+    //     }
+    //   }, []);
 
 
     const openInNewTab = (url : any) => {
@@ -37,12 +37,12 @@ const Linkpage = () => {
     return (
         <div className="flex flex-col justify-center pt-[10%] px-[10%]">
             <label className='text-purple2 text-base text-left'>Click this link to get started!</label> 
-            <button className="text-purple3 text-xs border border-purple4 mt-[15%] mb-[15%]" onClick={() => openInNewTab("https://t.me/insyncweb3_bot")} >https://t.me/insyncweb3_bot</button>
-            {address ? 
+            <button className="text-purple3 text-xs border border-purple4 mt-[30%] mb-[15%]" onClick={() => openInNewTab("https://t.me/insyncweb3_bot")} >https://t.me/insyncweb3_bot</button>
+            {/* {address ? 
             <label className='text-purple2 text-base'>Active EOA Address: {(address)?.slice(0,7)}...{(address)?.slice(36)}</label> : 
             <p className='text-purple2 text-base'>No connected wallet</p>
-            }
-            <label className='text-grey1 mt-[12%] text-xs'>@Powered by glyde</label>
+            } */}
+            <label className='text-grey1 mt-[30%] text-xs'>@Powered by glyde</label>
         </div>
     )
 }
